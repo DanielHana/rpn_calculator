b51ba806fa07afcde4debef314bc7e319e57c177
DanielHana <danielaprimbaz@gmail.com>
2022-07-09 16:19:07 -0700

    Set console color

--- rpn_calculator/RPN.CommandLine.UI/Program.cs
+++ rpn_calculator/RPN.CommandLine.UI/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Make the console blue because i like blue
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.White;
+
+
+Console.WriteLine("Hello, World!");
