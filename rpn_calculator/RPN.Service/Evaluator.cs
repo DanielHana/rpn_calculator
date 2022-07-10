92687e4e22e5666c072b366318be738364a4803c
DanielHana <danielaprimbaz@gmail.com>
2022-07-09 18:02:06 -0700

    Add Evaluator

---
+++ rpn_calculator/RPN.Service/Evaluator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPN.Service
+{
+    public class Evaluator
+    {
+        public double Evaluate(Stack<string> container)
+        {
+            string topCharacter = container.Pop();
+            double firstOperand, secondOperand;
+            
+            // if topCharacter is not a number, use it as the operator
+            if (!Double.TryParse(topCharacter, out firstOperand))
+            {
+                // the first operand pulled is actually the second in order of operation, since it was added to the stack later
+                secondOperand = Evaluate(container);
+                firstOperand = Evaluate(container);
+                firstOperand = PerformOperation(topCharacter, firstOperand, secondOperand);
+            }
+            return firstOperand;
+        }
+
+        public static double PerformOperation(string operation, double firstOperand, double secondOperand) => operation switch
+        {
+            "+" => firstOperand + secondOperand,
+            "-" => firstOperand - secondOperand,
+            "*" => firstOperand * secondOperand,
+            "/" => firstOperand / secondOperand,
+            _ => throw new InvalidOperationException($"Invalid operation performed, attempted the operation \"{firstOperand} {operation} {secondOperand}\""),
+        };
+    }
+}
