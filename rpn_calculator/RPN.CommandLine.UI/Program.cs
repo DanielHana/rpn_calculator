dfb49603f1151a183a70ecd1c0d8ae7d3b7e08d6
DanielHana <danielaprimbaz@gmail.com>
2022-07-09 21:06:27 -0700

    Add UI functionality for parsing and evaluating

--- rpn_calculator/RPN.CommandLine.UI/Program.cs
+++ rpn_calculator/RPN.CommandLine.UI/Program.cs
@@ -1,7 +1,41 @@
-﻿// Make the console blue because i like blue
+﻿using RPN.Service;
+
+// Make the console blue because i like blue
 Console.BackgroundColor = ConsoleColor.Blue;
 Console.Clear();
 Console.ForegroundColor = ConsoleColor.White;
 
+var evaluator = new Evaluator();
+var parser = new Parser();
+var exitOptions = new string[] { "q", "\\n", "", Environment.NewLine, "exit", "q!:", "x" };
 
-Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.Write("> ");
+    string? userInput = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(userInput) || exitOptions.Contains(userInput.ToLower())) break;
+
+    parser.Add(userInput);
+
+    if(parser.IsReadyForOperation)
+    {
+        double result = evaluator.Evaluate(parser.Characters);
+        parser.Add(result.ToString());
+        Console.WriteLine(result);
+    }
+    else if(parser.Errors.Count > 0)
+    {
+        foreach (var error in parser.Errors)
+        {
+            Console.WriteLine(error);
+        }
+        Console.WriteLine("Clearing stack, please try a different input");
+        parser.ResetParser();
+    }
+    else
+    {
+        Console.WriteLine(userInput);
+    }
+}
+
