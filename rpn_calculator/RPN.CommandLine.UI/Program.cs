5841fa27ae49045d264ec1da28d955fb499b75e2
DanielHana <danielaprimbaz@gmail.com>
2022-07-10 13:20:12 -0700

    Add QOL features to UI, allow user to clear console and list items in stack

--- rpn_calculator/RPN.CommandLine.UI/Program.cs
+++ rpn_calculator/RPN.CommandLine.UI/Program.cs
@@ -8,6 +8,11 @@ Console.ForegroundColor = ConsoleColor.White;
 var evaluator = new Evaluator();
 var parser = new Parser();
 var exitOptions = new string[] { "q", "\\n", "", Environment.NewLine, "exit", "q!:", "x" };
+var clearOptions = new string[] { "clear" };
+var listOptions = new string[] { "list", "ls" };
+
+Console.WriteLine("Please input an equation in Reverse Polish Notation");
+ListSupportedOperators(false);
 
 while (true)
 {
@@ -28,15 +33,35 @@ while (true)
     {
         Console.Clear();
         foreach (var error in parser.Errors)
-        {
             Console.WriteLine(error);
-        }
-        Console.WriteLine("Caclulator cleared, please try a different input");
+        
+        ListSupportedOperators(true);
         parser.ResetParser();
     }
     else
-    {
+    { 
         Console.WriteLine(userInput);
+    }
+
+    if (clearOptions.Contains(userInput.ToLower()))
+    {
+        Console.Clear();
+        parser.ResetParser();
+        ListSupportedOperators(true);
+    }
+
+    if (listOptions.Contains(userInput))
+    { 
+        Console.WriteLine("Current Stack:");
+        foreach (var a in parser.Characters)
+            Console.WriteLine(a);
     }
 }
 
+void ListSupportedOperators(bool clearingConsole)
+{
+    if (clearingConsole) Console.WriteLine("Caclulator cleared.");
+    Console.WriteLine("Please input an equation in Reverse Polish Notation");
+    Console.WriteLine("Supported Operaters: ");
+    Array.ForEach(Constants.ValidOperations, Console.WriteLine);
+}
