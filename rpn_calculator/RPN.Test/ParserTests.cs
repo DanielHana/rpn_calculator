a99faede71cf94fbd3ff5f8f00846033b1c7139d
DanielHana <danielaprimbaz@gmail.com>
2022-07-09 22:05:19 -0700

    Add parser tests

---
+++ rpn_calculator/RPN.Test/ParserTests.cs
@@ -0,0 +1,70 @@
+using RPN.Service;
+
+namespace RPN.Test
+{
+    public class ParserTests
+    {
+        [Test]
+        public void IsSingleNumber_InputIs2_ReturnTrue()
+        {
+            var parser = new Parser();
+
+            parser.Add("2");
+
+            var topCharacter = parser.Characters.Pop();
+            Assert.True(Double.Parse(topCharacter) == 2, "Parser should be able to handle single int");
+        }
+
+        [Test]
+        public void IsDecimalNumber_InputIs3_ReturnTrue()
+        {
+            var parser = new Parser();
+
+            parser.Add("3.0");
+
+            var topCharacter = parser.Characters.Pop();
+            Assert.True(Double.Parse(topCharacter) == 3.0, "Parser should be able to handle single double");
+        }
+
+        [Test]
+        public void FilterOutText_ReturnsTrue()
+        {
+            var parser = new Parser();
+
+            parser.Add("3 aserfdas");
+
+            var topCharacter = parser.Characters.Pop();
+            Assert.True(Double.Parse(topCharacter) == 3, "Parser should be able to filter out basic text");
+        }
+
+        [Test]
+        public void DetectsBasicInfix_ReturnsTrue()
+        {
+            var parser = new Parser();
+
+            parser.Add("2 + 2");
+
+            Assert.True(parser.Errors.Count > 0, "Parser should add an error if it detects Infix");
+        }
+
+        [Test]
+        public void DetectsBasicInvalidOperand_ReturnsTrue()
+        {
+            var parser = new Parser();
+
+            parser.Add("+");
+
+            Assert.True(parser.Errors.Count > 0, "Parser should detect basic lack of operands and add an error");
+        }
+
+        [Test]
+        public void DetectsNotEnoughOperans_ReturnsTrue()
+        {
+            var parser = new Parser();
+
+            parser.Add("1 +");
+
+            Assert.True(parser.Errors.Count > 0, "Parser should detect basic if there are too many operands to perform an operation");
+        }
+    }
+}
